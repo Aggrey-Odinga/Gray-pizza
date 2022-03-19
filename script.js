c913e2742e7dc1cd079344e72ad3503b7455d013
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-03-19 13:59:03 +0300

    Add media query features to enhance responsiveness of page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let searchbtn = document.querySelector('search');
-let searchfrm = document.querySelector('.head ,search-bar');
+let searchbtn = document.querySelector('#search');
+let searchfrm = document.querySelector('.head .search-bar');
 
 searchbtn.onclick = () => {
     searchfrm.classList.toggle('active');
