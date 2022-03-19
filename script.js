535e99476704ccb8937bfde1ff2c7bb1a5d70723
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-03-19 14:12:15 +0300

    Ft: Add jquery features to toggle menu on small screen

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ let searchfrm = document.querySelector('.head .search-bar');
 
 searchbtn.onclick = () => {
     searchfrm.classList.toggle('active');
+    menu.classList.remove('active');
+}
+// home toggle effect
+let menu = document.querySelector('.head .navbar');
+document.querySelector('#bars').onclick = () => {
+    menu.classList.toggle('active');
+    searchfrm.classList.remove('active');
 }
